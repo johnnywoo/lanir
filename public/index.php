508ce03e0297b053d9d8241477d5ea973a845a68
johnnywoo <agalkin@agalkin.ru>
2012-11-08 15:35:38 +0400

    More todo

--- public/index.php
+++ public/index.php
@@ -72,6 +72,7 @@ $only_load_once   = !empty($_REQUEST['only_load_once']);
 
 
 // CANDY:
+// counters are displayed wrong in chrome
 // when moving a big token, the arrow should be positioned relative to its center
 // big token without image should have a nice huge letter in it
 // minimum zoom out
@@ -84,6 +85,7 @@ $only_load_once   = !empty($_REQUEST['only_load_once']);
 // token representation separate from tokens themselves (pick image by name/keywords/properties etc)
 // list of standard unicode/shape-based token representations (for status effects, actual tokens, etc)
 // optionally drawing a grid (color in the map settings) as one element (or at most w+h instead of w*h); we need the grid to be visible to align it to the drawn grid on the map
+// doctoring of the applied log? we can have a very large amount of move commands because every select is a move, so the log will tend to become enormous (and we need the moves to keep proper z-index on tokens)
 
 // dealing with multiple tokens on the same square:
 // mark such squares with some icon (how do we deal with big tokens?)
