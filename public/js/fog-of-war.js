358cbf8b9a21b441abdb3a03e1fc310f08791e8a
johnnywoo <agalkin@agalkin.ru>
2012-11-13 08:20:52 +0400

    Code style fixes

--- public/js/fog-of-war.js
+++ public/js/fog-of-war.js
@@ -5,17 +5,15 @@ var FogOfWar = function(options) {
 	this.map        = null;
 	$.extend(this, options || {});
 
-	if(this.$container == null) {
-		throw 'No element to draw the fog of war into!';
-	}
+	var size = 100; // this is essentially graphics quality; actual onscreen size is managed by css (1 em = 1 cell)
 
 	var t = this;
 
-	var $canvas = $('<canvas class="fog-of-war" />');
-	this.$container.append($canvas);
-	var ctx = $canvas[0].getContext('2d');
 
-	var size = 100; // this is essentially graphics quality; actual onscreen size is managed by css (1 em = 1 cell)
+
+	//
+	// PUBLIC INTERFACE
+	//
 
 	this.toggle = function() {
 		$canvas.toggle();
@@ -27,23 +25,40 @@ var FogOfWar = function(options) {
 			$.merge(visibleArea, token.getVisibleArea());
 		});
 
-		// resetting the size erases the canvas, which is quite fortunate
 		var w = t.map.size[0];
 		var h = t.map.size[1];
-		$canvas.attr({
-			width:  size * w,
-			height: size * h
-		}).css({
-			width:  w + 'em',
-			height: h + 'em'
-		});
+
+		$canvas
+			// resetting the size erases the canvas, which is quite fortunate
+			.attr({
+				width:  size * w,
+				height: size * h
+			})
+			.css({
+				width:  w + 'em',
+				height: h + 'em'
+			});
 
 		for(var x = 0; x < w; x++) {
 			for(var y = 0; y < h; y++) {
-				if($.inArray((x + '_' + y), visibleArea) == -1) {
+				if($.inArray(x + '_' + y, visibleArea) == -1) {
 					ctx.fillRect(x * size, y * size, size, size);
 				}
 			}
 		}
 	};
+
+
+
+	//
+	// INITIALIZATION
+	//
+
+	if(this.$container == null) {
+		throw 'No element to draw the fog of war into!';
+	}
+
+	var $canvas = $('<canvas class="fog-of-war" />');
+	this.$container.append($canvas);
+	var ctx = $canvas[0].getContext('2d');
 };
